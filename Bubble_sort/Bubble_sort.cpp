79a1ff32b8fdf68a0bf56e5102215a86ce6435ed
FarhanVirenze <farhanvirenze18@gmail.com>
2023-03-16 21:31:40 +0700

    perbaikan (kurang "=")

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -22,7 +22,7 @@ void input() {
     cout << "Masukkan Element Array" << endl;
     cout << "============================" << endl;
 
-    for (int i + 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cout << "Data ke-" << (i + 1) << ": ";
         cin >> a[i];
     }
