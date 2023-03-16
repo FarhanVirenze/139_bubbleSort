be449a10ad60327ef6cdbc77964e9db31bdfabff
FarhanVirenze <farhanvirenze18@gmail.com>
2023-03-16 21:28:19 +0700

    tambahan void display

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -17,10 +17,10 @@ void input() {
             cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
         }
     }
-    cout << end1;
-    cout << "============================" << end1;
-    cout << "Masukkan Element Array" << end1;
-    cout << "============================" << end1;
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "Masukkan Element Array" << endl;
+    cout << "============================" << endl;
 
     for (int i + 0; i < n; i++) {
         cout << "Data ke-" << (i + 1) << ": ";
@@ -41,15 +41,29 @@ void bubbleSortArray() {
 }
 
 void unsorted() {
-    cout << end1;
-    cout << "============================" << end1;
-    cout << "Element Array yang belum tersusun" << end1;
-    cout << "============================" << end1;
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "Element Array yang belum tersusun" << endl;
+    cout << "============================" << endl;
     for (int j = 0; j < n; j++) {
-        cout << a[j] << end;
+        cout << a[j] << endl;
     }
-    cout << end1;
+    cout << endl;
 }
+
+void display() {
+    cout << endl;
+    cout << "============================" << endl;
+    cout << "Element Array yang telah tersusun" << endl;
+    cout << "============================" << endl;
+    for (int j = 0; j < n; j++) {
+        cout << a[j] << endl;
+    }
+    cout << endl;
+}
+
+
+
 
 int main()
 {
