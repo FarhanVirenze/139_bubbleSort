25a66de1180757d3d944dc06da82272d842fa64b
FarhanVirenze <farhanvirenze18@gmail.com>
2023-03-16 21:13:08 +0700

    tambahan void input

--- Bubble_sort/Bubble_sort.cpp
+++ Bubble_sort/Bubble_sort.cpp
@@ -2,6 +2,31 @@
 //
 
 #include <iostream>
+using namespace std;
+int a[20];
+int n;
+
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array: ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+        }
+    }
+    cout << end1;
+    cout << "============================" << end1;
+    cout << "Masukkan Element Array" << end1;
+    cout << "============================" << end1;
+
+    for (int i + 0; i < n; i++) {
+        cout << "Data ke-" << (i + 1) << ": ";
+        cin >> a[i];
+    }
+}
 
 int main()
 {
